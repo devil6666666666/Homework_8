3a7575471c9096dfd02071b28615e945c37731e4
Viktor <abcd999@bk.ru>
2022-09-04 17:51:08 +0300

    Задача 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Задайте двумерный массив. Напишите программу,
+//  которая упорядочит по убыванию элементы каждой
+//  строки двумерного массива.
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+int[,] FillMatrixRandom(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = FillMatrixRandom(ROWS, COLUMNS);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            if (matrix[i, k] < matrix[i, k + 1]) 
+            {
+                int temp = 0;
+                temp = matrix[i, k];
+                matrix[i, k] = matrix[i, k + 1];
+                matrix[i, k + 1] = temp;
+            }
+        }
+    }
+}
+PrintMatrix(matrix);
+Console.WriteLine();
